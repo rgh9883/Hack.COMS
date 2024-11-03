6254899e710b0f56beeff48866247d04880249a6
Christopher <cpw9783@rit.edu>
2024-11-03 03:40:00 -0500

    login page finished

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -7,8 +7,34 @@ import * as bootstrap from 'bootstrap'
 function Login(){
     return (
         <div>
-            <div class="d-flex bg-warning">
-                <h1>Sign in</h1>
+            <div class="d-flex flex-row w-100 vh-100">
+                <div class="d-flex flex-column w-100 justify-content-center align-items-center">
+                    <div class="d-flex justify-content-center w-100 mt-5">
+                        <h1>Sign In</h1>
+                    </div>
+                    <div class="d-flex justify-content-center w-100 mt-2">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    <div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Enter your username" aria-label="Username"></input>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="Enter your password" aria-label="Passward"></input>
+                        </div>
+                    </div>
+                    <div class="mb-2">
+                        <button type="button" class="btn btn-primary btn-lg">Sign In</button>
+                    </div>
+                    <div>
+                        <button type="button" class="btn btn-primary btn-lg">Sign Up</button>
+                    </div>
+                </div>
+                <div class="d-flex flex-column w-100 vh-100">
+                    <div class="d-flex justify-content-center align-items-center vh-100" >
+                        <img src="./src/hack.coms.logo.png" alt="teachmelogo" width="594" height="531"></img>
+                    </div>
+                </div>
             </div>
         </div>
     );
