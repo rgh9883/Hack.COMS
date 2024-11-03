282d36dab6020690df387ab522a300067fbce5bb
Christopher <cpw9783@rit.edu>
2024-11-03 02:39:29 -0500

    added scss and bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,15 +6,16 @@ import {
     BrowserRouter,
     Routes,
   } from "react-router-dom";
-import Users from "./pages/Users";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    <Route path="/dashboard" element={<Dashboard/>}/>
                     <Route path="/" element={<Login/>}/>
+                    <Route path="/dashboard" element={<Dashboard/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,17 @@
-
+// Import our custom CSS
+import '../scss/styles.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 
 function Login(){
-
+    return (
+        <div>
+            <div class="d-flex bg-warning">
+                <h1>Sign in</h1>
+            </div>
+        </div>
+    );
 }
 
 export default Login
