4ffc1eed0d40c94525cd53a795dc07f4ef3ec96b
Rahul Hegde <rgh9883@rit.edu>
2024-11-03 03:41:21 -0500

    worked on dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,42 @@
+import * as bootstrap from 'bootstrap';
+import { useEffect, useState, props } from 'react';
+import axios from 'axios';
 
+function Dashboard(props){
+    const [requests, setRequests] = useState([]);
+    
+    useEffect(() => {
+        const fetchAllRequests = async () => {
+            try {
+                const res = await axios.get(`http://localhost:3000/user?username=${props.username}`);
+                setRequests(res.data.requests);
+                console.log(requests);
+            } catch(err) {
+                console.log(err);
+            }
+        }
+        fetchAllRequests()
+    }, [])
 
-function Dashboard(){
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
+        <>
+            <h1>{props.username}'s Requests</h1>
+            <div className='row row-cols-4 row-cols-md-4'>
+                {requests.map(request => (
+                    <div className="col">
+                    <div className="card" style={{"width": 10 + "rem"}}>
+                        <div className="card-header">
+                            {request.request_type}
+                        </div>
+                        <ul className="list-group list-group-flush">
+                        <li className="list-group-item">{request.request_status}</li>
+                        <li className="list-group-item"><button className='btn btn-danger' id={request.request_id}>Delete</button></li>
+                        </ul>
+                  </div>
+                  </div>
+                ))}
+            </div>
+        </>
     );
 }
 export default Dashboard
